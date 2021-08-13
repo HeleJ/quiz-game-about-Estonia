cf4e2c3471ac956c8f449879ab21a3b3a137758d
HeleJ <hele.juhanson@gmail.com>
2021-08-13 08:32:29 +0000

    Add structure to questions file

---
+++ assets/js/questions.js
@@ -0,0 +1,59 @@
+//creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+        numb: 1,
+        question:"Where is Estonia located?",
+        answer:"Northen Europe",
+        options:[
+            "Asia",
+            "Northen Europe",
+            "America",
+            "Africa"
+        ]
+    },
+    {
+        numb: 2,
+        question:"",
+        answer:"",
+        options:[
+            "",
+            "",
+            "",
+            ""
+        ]
+    },
+    {
+        numb: 3,
+        question:"",
+        answer:"",
+        options:[
+            "",
+            "",
+            "",
+            ""
+        ]
+    },
+    {
+        numb: 4,
+        question:"",
+        answer:"",
+        options:[
+            "",
+            "",
+            "",
+            ""
+        ]
+    },
+    {
+        numb: 5,
+        question:"",
+        answer:"",
+        options:[
+            "",
+            "",
+            "",
+            ""
+        ]
+    },
+
+]
